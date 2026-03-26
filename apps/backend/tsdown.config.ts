d1d9e9b9a5b1abd4d8054f9e09b4cc252beca2f5
Leo42night <karmaborutovvo@gmail.com>
2026-03-27 04:41:18 +0700

    be: err, update tsconfig add external [] fill with prisma

--- apps/backend/tsdown.config.ts
+++ apps/backend/tsdown.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+  external: [
+    '@prisma/client',
+    '.prisma/client',
+    '@prisma/adapter-libsql',
+    '@libsql/client'
+  ],
   deps: {
     alwaysBundle: ['shared']
   }
