734de6f872e0c059a5efa67fd0c099cf93e713d2
Leo42night <karmaborutovvo@gmail.com>
2026-03-27 10:47:34 +0700

    be: err, reset bun.lock

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,6 +1,5 @@
 import { PrismaLibSql } from "@prisma/adapter-libsql";
-// import { PrismaClient } from "../src/generated/prisma/client";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../src/generated/prisma/client";
 
 const adapter = new PrismaLibSql({
   url: process.env.DATABASE_URL!,
