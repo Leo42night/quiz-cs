13e9a6abc32ae034460631cfde0de0df993d86e0
Leo42night <karmaborutovvo@gmail.com>
2026-03-27 10:38:16 +0700

    pakai @prisma/client

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,5 +1,6 @@
 import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { PrismaClient } from "../src/generated/prisma/client";
+// import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaLibSql({
   url: process.env.DATABASE_URL!,
--- apps/backend/tsdown.config.ts
+++ apps/backend/tsdown.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   deps: {
     // bundling file diluar root folder proyek (vercel -> apps/backend)
-    alwaysBundle: ['shared'],
-    // Tandai path generated sebagai external agar tetap dicari di filesystem saat runtime
-    neverBundle: ['../src/generated/prisma/client']
+    alwaysBundle: ['shared']
   }
 })
